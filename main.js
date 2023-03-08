df7700c4d3e0cfcfa82e86484a927f89934cb505
Djafer06 <huferdzufer@gmail.com>
2023-03-08 22:28:48 +0300

    15

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let str = 'Hello';
+alert(str + '!!!');
+
+let str2 = 'Java';
+let str3 = 'script';
+alert('Now im learning ' + str2 + str3);
+
+let str4 = 'Hello';
+let str5 = 'World';
+alert(str4 + ' ' + str5);
