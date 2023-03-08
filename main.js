3c18c1ff69591e7f8e7fd38b2418f85603d7e88b
Djafer06 <huferdzufer@gmail.com>
2023-03-08 19:49:55 +0300

    06

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let num = 123;
+alert(num)
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+let d = 4, e = 5, f = 6;
+
+let g;
+g = 7;
+
+alert('num = ' + num);
+let ab = 10;
+alert('ab = ' + ab);
+ab = 20;
+alert('ab = ' + ab); 
+/*
+	многострочный комментарий
+*/
