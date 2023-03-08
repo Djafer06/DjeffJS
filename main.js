89cc9b83dd7cd8c6676011114c7627fe6e5bc19d
Djafer06 <huferdzufer@gmail.com>
2023-03-08 19:53:14 +0300

    08

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let a = 10;
+let b = 2;
+alert('1 task. sum = ' + a + b);
+alert('1 task. diff = ' + a - b);
+alert('1 task. multiple = ' + a * b);
+alert('1 task. div = ' + a / b); 
+
+let c = 10;
+let d = 5;
+let result = c + d;
+alert('2 task. result =' + result);
+
+let e = 1;
+let f = 2;
+let g = 3;
+alert('3 task. sum = ' + (e + f + g));
+
+let h = 10;
+let i = 5;
+let j = h - i;
+let m = 7;
+let n = j + m;
+alert('4 task. result = ' + n);
+/*
+	многострочный комментарий
+*/
