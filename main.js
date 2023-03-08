08d2d2a0bd99953948f7d57454911acd51e0a3f8
Djafer06 <huferdzufer@gmail.com>
2023-03-08 22:33:32 +0300

    19

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let num = 123;
+console.log(num);
+
+let num1 = 123;
+let num2 = 456;
+let num3 = 789;
+console.log(num1, num2, num3);
+
+console.log(123);
+console.log('123'); // строка выводится черным цветом
+
+alert(eee); // ошибка
