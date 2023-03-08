ca5a1f878201331056d5a8cf55c6f15c141b2e0d
Djafer06 <huferdzufer@gmail.com>
2023-03-08 22:32:08 +0300

    18

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let a;
+alert(a);
+
+let b = null;
+alert(b);
+
+let c = true;
+alert(c);
+
+let d = false;
+let(d);
+
+let str = 'hello';
+let str1 = 'world';
+alert(str * str1);
+
+alert(10 / 0);
+alert(-10 / 0);
